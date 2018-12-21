191ded078a49ba4b36422ea082a7da1114aa7dbb
vicksbr <pedropuzzi@gmail.com>
2018-12-21 08:20:57 -0200

    sprint criada com apenas o boilerplate minimo necessario para a aplicação flask-react

--- backend/server/config/__init__.py
+++ backend/server/config/__init__.py
@@ -4,17 +4,7 @@ config = {
         "port": 27017,
         "db": "basic_api_data"
     },
-    "LOGIN_SERVICE": {
-        "client_id": "905832",
-        "client_secret": "thkpvfo5kqsldne0yvperokm8x3ljqwt",
-        "login_host": "https://login-stage.raccoon.ag",
-        "redirect_uri": "http://localhost:3134/",
-    },
     "APP": {
         "SECRET_KEY": ""
     },
-    "SUPER_ADMIN": {
-        "EMAIL": "ti@raccoon.ag"
-    },
-    "GTM": "",
 }
